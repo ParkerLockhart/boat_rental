4fbbcf210cc01df815c9cfa29c8dc95a2a1c5a0d
Parker <parkerlockhart42@gmail.com>
2021-11-06 16:19:33 -0500

    Add rent and charge methods

--- lib/dock.rb
+++ lib/dock.rb
@@ -2,10 +2,27 @@ require './lib/renter'
 require './lib/boat'
 
 class Dock
-  attr_reader :name, :max_rental_time
+  attr_reader :name, :max_rental_time, :rental_log
 
   def initialize(name, max_rental_time)
     @name = name
     @max_rental_time = max_rental_time
-  end 
+    @rental_log = {}
+  end
+
+  def rent(boat, renter)
+    @rental_log.store(boat, renter)
+  end
+
+  def charge(boat)
+    charge = {}
+    if boat.hours_rented > @max_rental_time
+      boat.hours_rented = @max_rental_time
+    end 
+    fee = boat.hours_rented * boat.price_per_hour
+    charge.store(:card_number, @rental_log[boat].credit_card_number)
+    charge.store(:amount, fee)
+    charge
+  end
+
 end
