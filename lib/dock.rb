a223dd489c042b44b8202d6a2f57f3e01802bf62
Parker <parkerlockhart42@gmail.com>
2021-11-06 15:10:38 -0500

    Dock initialize method

---
+++ lib/dock.rb
@@ -0,0 +1,11 @@
+require './lib/renter'
+require './lib/boat'
+
+class Dock
+  attr_reader :name, :max_rental_time
+
+  def initialize(name, max_rental_time)
+    @name = name
+    @max_rental_time = max_rental_time
+  end 
+end
